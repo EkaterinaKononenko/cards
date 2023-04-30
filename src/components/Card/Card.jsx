7b7ce8c79b2a4e6fda1bb4771a08aba4912782a6
EkaterinaKononenko <kaktysik4@ukr.net>
2023-04-30 21:55:00 +0300

    add buttons

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,22 +9,53 @@ import {
   Followers,
   Btn
 } from './Card.styled';
+import { useState, useEffect } from 'react';
 
 const Card = ({ id, user, tweets, avatar, followers }) => {
-    return (
-      <>
-        <Item key={id}>
-          <ImgGoIt src="images/goit.png" alt="goit" />
-          <ImgSign src="images/sign.png" alt="goit" />
-          <ImgLine src="images/line.png" alt="line" />
-          <ImgEllipse src="images/ellipse.png" alt="ellipse" />
-          <ImgAvatar src={avatar} alt={user} />
-          <TweetsText>{tweets} TWEETS</TweetsText>
-          <Followers>{followers} FOLLOWERS</Followers>
-          <Btn type="button">FOLLOW</Btn>
-        </Item>
-      </>
-    );
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [followersCount, setFollowersCount] = useState(0);
+
+   useEffect(() => {
+     const savedFollowers = localStorage.getItem(`followersCount_${id}`);
+     if (savedFollowers) {
+       setFollowersCount(parseInt(savedFollowers));
+     } else {
+       setFollowersCount(followers);
+     }
+   }, [id, followers]);
+
+  const handleFollowClick = () => {
+    setIsFollowing(!isFollowing);
+    setFollowersCount(prevCount => {
+      const newCount = prevCount + (isFollowing ? -1 : 1);
+      localStorage.setItem(`followersCount_${id}`, newCount);
+      return newCount;
+    });
+  };
+
+  const formattedCount = followersCount.toLocaleString();
+
+  return (
+    <>
+      <Item key={id}>
+        <ImgGoIt src="images/goit.png" alt="goit" />
+        <ImgSign src="images/sign.png" alt="goit" />
+        <ImgLine src="images/line.png" alt="line" />
+        <ImgEllipse src="images/ellipse.png" alt="ellipse" />
+        <ImgAvatar src={avatar} alt={user} />
+        <TweetsText>{tweets} TWEETS</TweetsText>
+        <Followers> {formattedCount} followers</Followers>
+        <Btn
+          onClick={handleFollowClick}
+          style={{
+            backgroundColor: isFollowing ? '#5CD3A8' : '#ebd8ff',
+          }}
+        >
+          {isFollowing ? 'FOLLOWING' : 'FOLLOW'}
+        </Btn>
+      </Item>
+    </>
+  );
 }
 
 export default Card;
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,16 @@
 import { NotFoundWrapper, NotFoundImg, NotFoundBtn } from './NotFound.styled';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+   const navigate = useNavigate();
+   const location = useLocation();
+   const onGoBackClick = () => {
+     navigate(location?.state?.from ?? '/');
+   };
   return (
     <>
       <NotFoundWrapper>
-        <NotFoundBtn>&larr; GO BACK</NotFoundBtn>
+        <NotFoundBtn type="button" onClick={onGoBackClick}>&larr; GO BACK</NotFoundBtn>
         <NotFoundImg src="images/notFound.png" alt="notFound" />
       </NotFoundWrapper>
     </>
